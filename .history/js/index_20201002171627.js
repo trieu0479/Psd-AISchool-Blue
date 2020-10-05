238fbfed3ea90249a29801218c2c9cf774661745
trieu0479 <trieu0479@gmail.com>
2020-10-05 11:17:22 +0700

    first commit

---
+++ .history/js/index_20201002171627.js
@@ -0,0 +1,56 @@
+
+// ====================================== slick js=============
+
+$(document).ready(function(){
+  $('.section__life-sportslider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    nextArrow: '<div class="nextArrowBtn"><img src="./images/next_icon.png"/></div>',
+    prevArrow: '<div class="prevArrowBtn"><img src="./images/prev_icon.png"/></span>',
+    slidesToShow: 3,
+  responsive: [    
+    // {
+    //   breakpoint: 480,
+    //   settings: {       
+    //     centerMode: true,       
+    //   }
+    // }
+  ]
+  });
+  $('.section__talk__circle').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    nextArrow: '<div class="nextArrowBtn"><img src="./images/next_icon.png"/></div>',
+    prevArrow: '<div class="prevArrowBtn"><img src="./images/prev_icon.png"/></span>'
+  });
+  $('.section__background-parent').slick({
+    dots:true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+  });
+  $('.header__menures').hide();
+$('.clickbar').click (()=> {
+ 
+  $(".header__menures").slideToggle();
+
+})
+$('.show__form').click (()=> {
+  
+  $(".section__background-form").slideToggle();
+
+})
+
+  $( function() {
+    $("#tabs").tabs();
+  } );
+
+
+})
